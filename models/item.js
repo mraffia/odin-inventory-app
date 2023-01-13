503b5fc0d381471658e1ddaf8be00c67e47096dd
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-13 18:25:06 +0700

    Create Item model

---
+++ models/item.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  book: { type: Schema.Types.ObjectId, ref: "Category", required: true }, // reference to the associated book
+  name: { type: String, required: true },
+  desc: { type: String, required: true },
+  price: { type: Number, required: true, min: 0, default: 0 },
+  number_in_stock: { type: Number, required: true, min: 0, default: 0 },
+});
+
+// Virtual for item's URL
+ItemSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/item/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Item", ItemSchema);
