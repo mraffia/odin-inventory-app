c6a413e8a6de796d1d7db804e1a17d4b304ccf5a
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-14 03:11:43 +0700

    Add next to all controller parameters

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,45 +1,45 @@
 const Category = require("../models/category");
 
-exports.index = (req, res) => {
+exports.index = (req, res, next) => {
   res.send("NOT IMPLEMENTED: Site Home Page");
 };
 
 // Display list of all Categories.
-exports.category_list = (req, res) => {
+exports.category_list = (req, res, next) => {
   res.send("NOT IMPLEMENTED: Category list");
 };
 
 // Display detail page for a specific Category.
-exports.category_detail = (req, res) => {
+exports.category_detail = (req, res, next) => {
   res.send(`NOT IMPLEMENTED: Category detail: ${req.params.id}`);
 };
 
 // Display Category create form on GET.
-exports.category_create_get = (req, res) => {
+exports.category_create_get = (req, res, next) => {
   res.send("NOT IMPLEMENTED: Category create GET");
 };
 
 // Handle Category create on POST.
-exports.category_create_post = (req, res) => {
+exports.category_create_post = (req, res, next) => {
   res.send("NOT IMPLEMENTED: Category create POST");
 };
 
 // Display Category delete form on GET.
-exports.category_delete_get = (req, res) => {
+exports.category_delete_get = (req, res, next) => {
   res.send("NOT IMPLEMENTED: Category delete GET");
 };
 
 // Handle Category delete on POST.
-exports.category_delete_post = (req, res) => {
+exports.category_delete_post = (req, res, next) => {
   res.send("NOT IMPLEMENTED: Category delete POST");
 };
 
 // Display Category update form on GET.
-exports.category_update_get = (req, res) => {
+exports.category_update_get = (req, res, next) => {
   res.send("NOT IMPLEMENTED: Category update GET");
 };
 
 // Handle Category update on POST.
-exports.category_update_post = (req, res) => {
+exports.category_update_post = (req, res, next) => {
   res.send("NOT IMPLEMENTED: Category update POST");
 };
