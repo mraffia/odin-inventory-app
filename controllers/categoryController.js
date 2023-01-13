5cd780aa903d84ce42a17ae514844c033e0c4084
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-14 03:37:37 +0700

    Add logic to index controller that fetch information about how many records are available in db and render the data in a template, also add logic to category_list controller to get a list of all Category objects in db, sort them, and then pass these to the template for rendering

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,12 +1,39 @@
 const Category = require("../models/category");
+const Item = require("../models/item");
+
+const async = require("async");
 
 exports.index = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Site Home Page");
+  async.parallel(
+    {
+      category_count(callback) {
+        Category.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
+      },
+      item_count(callback) {
+        Item.countDocuments({}, callback);
+      },
+    },
+    (err, results) => {
+      res.render("index", {
+        title: "Sly's Shop (Inventory App)",
+        error: err,
+        data: results,
+      });
+    }
+  );
 };
 
 // Display list of all Categories.
 exports.category_list = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category list");
+  Category.find({}, "name")
+    .sort({ name: 1 })
+    .exec(function (err, list_categories) {
+      if (err) {
+        return next(err);
+      }
+      //Successful, so render
+      res.render("category_list", { title: "Category List", category_list: list_categories });
+    });
 };
 
 // Display detail page for a specific Category.
