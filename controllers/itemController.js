13840d16f0109f1f5b6eb4c6990c1d0a1660a9dd
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-14 18:10:55 +0700

    Add logic to item_update_get and item_update_post controllers

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -187,10 +187,125 @@ exports.item_delete_post = (req, res, next) => {
 
 // Display Item update form on GET.
 exports.item_update_get = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item update GET");
+  // Get item and categories for form.
+  async.parallel(
+    {
+      item(callback) {
+        Item.findById(req.params.id)
+          .populate("category")
+          .exec(callback);
+      },
+      categories(callback) {
+        Category.find(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.item == null) {
+        // No results.
+        const err = new Error("Item not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Success.
+      // Mark our selected categories as checked.
+      for (const category of results.categories) {
+        for (const itemCategory of results.item.category) {
+          if (category._id.toString() === itemCategory._id.toString()) {
+            category.checked = "true";
+          }
+        }
+      }
+      res.render("item_form", {
+        title: "Update Item",
+        categories: results.categories,
+        item: results.item,
+      });
+    }
+  );
 };
 
 // Handle Item update on POST.
-exports.item_update_post = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item update POST");
-};
+exports.item_update_post = [
+  // Convert the category to an array
+  (req, res, next) => {
+    if (!Array.isArray(req.body.category)) {
+      req.body.category =
+        typeof req.body.category === "undefined" ? [] : [req.body.category];
+    }
+    next();
+  },
+
+  // Validate and sanitize fields.
+  body("item_name", "Name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("item_desc", "Description must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Price must not be below 0").escape(),
+  body("number_in_stock", "Stock must not be below 0").escape(),
+  body("category.*").escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Item object with escaped/trimmed data and old id.
+    const item = new Item({
+      name: req.body.item_name,
+      desc: req.body.item_desc,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+      category: typeof req.body.category === "undefined" ? [] : req.body.category,
+      _id: req.params.id, //This is required, or a new ID will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all categories for form.
+      async.parallel(
+        {
+          categories(callback) {
+            Category.find(callback);
+          },
+        },
+        (err, results) => {
+          if (err) {
+            return next(err);
+          }
+
+          // Mark our selected categories as checked.
+          for (const category of results.categories) {
+            if (item.category.includes(category._id)) {
+              category.checked = "true";
+            }
+          }
+          res.render("item_form", {
+            title: "Update Item",
+            categories: results.categories,
+            item,
+            errors: errors.array(),
+          });
+        }
+      );
+      return;
+    }
+
+    // Data from form is valid. Update the record.
+    Item.findByIdAndUpdate(req.params.id, item, {}, (err, theitem) => {
+      if (err) {
+        return next(err);
+      }
+
+      // Successful: redirect to item detail page.
+      res.redirect(theitem.url);
+    });
+  },
+];
